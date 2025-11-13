9d588a003e5e321b6de648b1b8b5cfe734f98163
olegzdanov34-vulki <olegzdanov34@gmail.com>
2025-11-13 19:30:49 +0500

    test commit-2

--- src/main/java/com/example/demogit/DemoGitApplication.java
+++ src/main/java/com/example/demogit/DemoGitApplication.java
@@ -9,6 +9,7 @@ public class DemoGitApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoGitApplication.class, args);
         System.out.println("The path to Junior!");
+        System.out.println("Stay hard");
     }
 
 }
